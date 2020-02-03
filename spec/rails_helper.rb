7c73d5635b50cc4408cc25cea44cbcfe31688752
mantanquzzmo <quzzmo@hotmail.com>
2020-02-03 14:11:19 +0100

    Revert "adds config and factory bot"
    This reverts commit d840beecb1b20d3910c602945370e7f73747d381.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,4 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-  config.include FactoryBot::Syntax::Methods
-  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
 end
