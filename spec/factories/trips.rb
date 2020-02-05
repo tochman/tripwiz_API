b8ef0d2057deff9e475b2b7db298f80c69a4797e
mantanquzzmo <quzzmo@hotmail.com>
2020-02-05 15:23:59 +0100

    makes changes to factorybot

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :trip do
     lat { "59.334" }
     lng { "18.063" }
-    name { "Stockholm" }
-    place_id { "ChIJywtkGTF2X0YRZnedZ9MnDag" }
+    destination { "Stockholm" }
   end
 end
