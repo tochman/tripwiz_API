3032f19c73c7583db9b475d6729876c747847d7c
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-02-06 17:08:22 +0100

    updates stubs for merged test

--- spec/requests/api/v1/trips/create_spec.rb
+++ spec/requests/api/v1/trips/create_spec.rb
@@ -19,11 +19,9 @@ RSpec.describe 'POST /api/v1/trips', type: :request do
   describe 'Unsuccesfully when missing coords' do
     before do
       get_geobytes_no_lat_lng
+
       post '/api/v1/trips',
       params: { days: 4 }
-    end
-
-      post '/api/v1/trips'
     end
 
     it 'returns a 422 response status' do
@@ -45,11 +43,6 @@ RSpec.describe 'POST /api/v1/trips', type: :request do
                 days: 4 }
     end
 
-      post '/api/v1/trips',
-           params: { lat: '9.3293',
-                     lng: '18.0685' }
-    end
-
     it 'returns a 422 response status' do
       expect(response).to have_http_status 422
     end
@@ -61,13 +54,15 @@ RSpec.describe 'POST /api/v1/trips', type: :request do
 
   describe 'Unsuccesfully when missing days' do
     before do
+      get_geobytes_success
+      
       post '/api/v1/trips',
       params: { lat: '59.3293',
                 lng: '18.0685' }
     end
 
 
-    it 'returns a 200 response status' do
+    it 'returns a 422 response status' do
       expect(response).to have_http_status 422
     end
 
