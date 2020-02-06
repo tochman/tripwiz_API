b0d327eea953bc975601226c8cee655b5c82c757
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-02-06 20:59:44 +0100

    Moves methods to model, API responds with activities

--- app/controllers/api/v1/activity_types_controller.rb
+++ app/controllers/api/v1/activity_types_controller.rb
@@ -2,67 +2,20 @@
 
 class Api::V1::ActivityTypesController < ApplicationController
   def create
-    @number = params[:number]
-    @activity_type = ActivityType.create(activity_type: params[:activity_type], trip_id: params[:trip])
+    number = params[:number]
+    activity_type = ActivityType.create(activity_type: params[:activity_type], trip_id: params[:trip])
 
-    if @activity_type.persisted?
-      response = create_activities
+    if activity_type.persisted?
+      activities = Activity.create_activities(activity_type, number)
 
-      if response && response.length == @number.to_i
-        render json: @activity_type, status: 200
+      if activities && activities.length == number.to_i
+        render json: activities, status: 200
       else
-        @activity_type.destroy
-        render json: { error: "Failed to create activity." }, status: 422
+        activity_type.destroy
+        render json: { error: 'Failed to create activity.' }, status: 422
       end
     else
-      render json: { error: @activity_type.errors.full_messages }, status: 422
+      render json: { error: activity_type.errors.full_messages }, status: 422
     end
-  end
-
-  private
-
-  def create_activities
-    activities = []
-    response = get_activities
-
-    if response['results'].length >= @number.to_i
-      (0..@number.to_i - 1).each do |i|
-        name = response['results'][i]['name']
-        rating = response['results'][i]['rating']
-        address = response['results'][i]['vicinity']
-        lat = response['results'][i]['geometry']['location']['lat']
-        lng = response['results'][i]['geometry']['location']['lng']
-        
-        activity = Activity.create(
-          name: name,
-          address: address,
-          rating: rating,
-          lat: lat,
-          lng: lng,
-          activity_type_id: @activity_type.id
-        )
-
-        if activity.persisted?
-          activities << activity
-        end
-      end
-
-      activities
-    else
-      nil
-    end
-  end
-
-  def get_activities
-    radius = 1000
-    lat = @activity_type.trip.lat
-    lng = @activity_type.trip.lng
-
-    url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
-    response = JSON.parse RestClient.get url,
-                                         params: { location: "#{lat},#{lng}",
-                                                   type: @activity_type.activity_type,
-                                                   radius: radius,
-                                                   key: Rails.application.credentials.google_api_token }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
     namespace :v1, defaults: { format: :json } do
       resources :trips, only: [:create]
       resources :activity_types, only: [:create]
-      resources :activities, only: [:create]
     end
   end
 end
