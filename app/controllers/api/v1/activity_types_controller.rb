f4a1f35e8f609c8172d193f72ce318ae1d583c07
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-02-07 10:47:05 +0100

    permits and requires params

--- app/controllers/api/v1/activity_types_controller.rb
+++ app/controllers/api/v1/activity_types_controller.rb
@@ -2,12 +2,13 @@
 
 class Api::V1::ActivityTypesController < ApplicationController
   def create
-    number_of_activities = params[:actTimes]
-    activity_type = ActivityType.create(activity_type: params[:activity_type], trip_id: params[:trip])
+    number_of_activities = permitted_params.require(:actTimes)
+    activity_type = ActivityType.create(activity_type: permitted_params.require(:activity_type),
+                                        trip_id: permitted_params.require(:trip))
 
     if activity_type.persisted?
       activities = Activity.create_activities(activity_type, number_of_activities)
-
+      binding.pry
       if activities && activities.length == number_of_activities.to_i
         render json: activities, status: 200
       else
@@ -17,5 +18,11 @@ class Api::V1::ActivityTypesController < ApplicationController
     else
       render json: { error: activity_type.errors.full_messages }, status: 422
     end
+  end
+
+  private
+
+  def permitted_params
+    params.permit(:activity_type, :actTimes, :trip)
   end
 end
